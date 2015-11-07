107c820d419d41752cacc64276d351bea648ed23
takerusekine <t.0327.s@gmail.com>
2015-11-07 21:39:36 +0900

    一時的に完成。あとは修正していく

---
+++ contact.php
@@ -0,0 +1,31 @@
+<section class="margin-t80">
+  <div class="container-center">
+    <p class="top-buffer small-text">
+      弊社へのお問い合わせについては、下記フォームよりご連絡ください。
+    </p>
+    <p class="top-buffer small-text">
+      サービスに関するお問い合わせは通常3営業日以内に担当者よりご連絡いたします。その他のお問い合わせは、内容によって回答にお時間がかかる場合があります。あらかじめご了承ください。
+    </p>
+    <div class="top-buffer">
+      <div class="row">
+        <div class="col-lg-6 align-center">
+          <a href="#" class="btn btn-lg font-bold small-heading">
+              サービスに関するお問い合わせ
+          </a>
+        </div>
+        <div class="col-lg-6 align-center">
+          <a href="#" class="btn btn-lg font-bold small-heading">
+              その他のお問い合わせ
+          </a>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</section>
+<section class="purewhitebg section-padding100">
+  <div class="container-center">
+  <?php echo do_shortcode('[contact-form-7 id="33" title="コンタクトフォーム 1"]'); ?>
+  </div>
+</section>
--- page.php
+++ page.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
+<?php if (is_page('contact')):?>
+	<section id="contents" class="contents-wrapper whitebg">
+<?php else: ?>
 	<section id="contents" class="contents-wrapper">
+<?php endif; ?>
 	<?php
     if (have_posts()):
                         while (have_posts()):
