2d6959ece0534156d59d4e0c98f4f0924c52f9af
takerusekine <t.0327.s@gmail.com>
2015-11-11 11:34:07 +0900

    global navi と　レイアウトの修正

--- content.php
+++ content.php
@@ -2,8 +2,34 @@
 
 			<header class="entry-header container-fluid">
 				<div class="row">
-				<h1 class="black-text condensed-bold small-heading top-buffer-pudding bottom-buffer-pudding col-xs-12"><?php the_title(); ?></h1>
+
+				<h1 class="black-text condensed-bold small-heading top-buffer-pudding bottom-buffer-pudding col-xs-12 col-sm-6">
+					<?php if (is_page('corporate-info') or is_page('access')):?>
+						<?php
+                                $parent_id = $post->post_parent;
+                                if ($parent_id) {
+                                    echo get_the_title($parent_id);
+                                }
+                                ?>
+					<?php else: ?>
+					<?php the_title(); ?>
+				<?php endif; ?>
+				</h1>
+				<?php if (is_page('corporate-info') or is_page('access')):?>
+					<div class="col-sm-6 top-buffer-pudding bottom-buffer-pudding">
+					<?php
+                    wp_nav_menu(
+                        array(
+                        'container'  => 'div',
+                        'container_class' => 'child-menu',
+                        'theme_location' => 'place_global2'
+                        )
+                        )
+                    ;?>
+					</div>
+									<?php endif; ?>
 				</div>
+
 			</header>
 		</div>
 			<div>
--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
@@ -23,31 +24,52 @@
 		?>
 	</title>
 	<!-- jQuery library (served from Google) -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:700,300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-<script src="<?php bloginfo('template_url');?>/js/jquery-1.11.3.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/owl.carousel.min.js"></script>
+	<script src="<?php bloginfo('template_url');?>/js/jquery-1.11.3.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/js/owl.carousel.min.js"></script>
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.carousel.min.css">
-<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.theme.default.min.css">
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
-<script>$(document).ready(function(){$('.owl-carousel').owlCarousel({loop:true,margin:30,nav:true,navText: ["<i class='fa fa-chevron-left' style='padding:0 2px'></i>","<i class='fa fa-chevron-right' style='padding:0 2px'></i>"],slideBy:1,dots:true,responsive:{0:{items:1},600:{items:1},}})});</script>
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
+	<script>
+		$(document).ready(function() {
+			$('.owl-carousel').owlCarousel({
+				loop: true,
+				margin: 30,
+				nav: true,
+				navText: ["<i class='fa fa-chevron-left' style='padding:0 2px'></i>", "<i class='fa fa-chevron-right' style='padding:0 2px'></i>"],
+				slideBy: 1,
+				dots: true,
+				responsive: {
+					0: {
+						items: 1
+					},
+					600: {
+						items: 1
+					},
+				}
+			})
+		});
+	</script>
 	<?php wp_head();?>
 </head>
+
 <body <?php body_class(); ?>>
 	<div class="container-center">
 		<div class="container-fluid">
-		<div class="row">
-		<?php if(is_front_page()):?>
-		<div class="menuwrapper" id="menuwrapper">
-		<?php else:?>
-			<div class="menuwrapper-lower" id="menuwrapper-lower">
-		<?php endif;?>
-		<div class="container-center">
+			<div class="row">
+				<?php if(is_front_page()):?>
+					<div class="menuwrapper" id="menuwrapper">
+						<?php else:?>
+							<div class="menuwrapper-lower" id="menuwrapper-lower">
+								<?php endif;?>
+									<div class="container-center">
 
-		<nav class="topmenu col-xs-6 col-sm-12 col-md-12 col-lg-12">
-			<a href="<?php echo site_url();?>">
-		 		<span class="white-text companyname condensed-bold small-heading">Bulluck</span>
-			</a>
-			<?php
+										<nav class="topmenu col-xs-6 col-sm-12 col-md-12 col-lg-12">
+											<a href="<?php echo site_url();?>">
+												<span class="white-text companyname condensed-bold small-heading">Bulluck</span>
+											</a>
+											<?php
 			wp_nav_menu(
 				array(
 				'container'  => '',
@@ -56,31 +78,31 @@
 				)
 				)
 			;?>
-		</nav>
-		</div>
+										</nav>
+									</div>
 
-	</div>
-	</div>
-</div>
-		</div>
-</div>
-
-	<?php if(is_front_page()):?>
-	<div class="keyvisual section-padding100 js-height">
-		<div class="kv-table">
-			<div class="container-fluid kv-table-cell">
-				<div class="row">
-					<h1 class="condensed-bold top-buffer-pudding top-buffer col-xs-12 col-sm-12 col-md-12 col-lg-12">Lorem ipsum dolor</h1>
-				</div>
-				<div class="row">
-					<h2 class="condensed-bold top-buffer small-heading col-xs-12 col-sm-8 col-md-8 col-lg-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</h2>
-				</div>
-				<div class="top-buffer row">
-					<p class="white-text col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-					</p>
-				</div>
+							</div>
+					</div>
 			</div>
 		</div>
 	</div>
-<?php endif;?>
+
+	<?php if(is_front_page()):?>
+		<div class="keyvisual section-padding100 js-height">
+			<div class="kv-table">
+				<div class="container-fluid kv-table-cell">
+					<div class="row">
+						<h1 class="condensed-bold top-buffer-pudding top-buffer col-xs-12 col-sm-12 col-md-12 col-lg-12">Lorem ipsum dolor</h1>
+					</div>
+					<div class="row">
+						<h2 class="condensed-bold top-buffer small-heading col-xs-12 col-sm-8 col-md-8 col-lg-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</h2>
+					</div>
+					<div class="top-buffer row">
+						<p class="white-text col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php endif;?>
