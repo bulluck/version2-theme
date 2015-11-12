8ee040f224dff35c10b0df2a21abd24351c1f245
takerusekine <t.0327.s@gmail.com>
2015-11-12 15:15:31 +0900

    768px以下でbufferを入れられるようにクラスを追加。

--- footer.php
+++ footer.php
@@ -43,12 +43,12 @@
   <div class="container-center">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-xs-6 col-lg-6">
+      <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
         <span class="small-heading condensed-bold">
           Bulluck Co., Ltd.
         </span>
       </div>
-      <div class="col-xs-4 col-lg-4">
+      <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 xs-t-buffer">
         <p class="small-text black-text">
         サイト制作/ランディングページ制作/アプリ開発/経営コンサルティング/プリシパルインベストメント
         </p>
