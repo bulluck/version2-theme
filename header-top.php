a7395db0756183fff16e9f74f42edc18d0fb2834
takerusekine <t.0327.s@gmail.com>
2015-11-20 02:07:23 +0900

    LPグローバルナビ修正済み

--- header-top.php
+++ header-top.php
@@ -31,7 +31,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/layout.css" media="screen" title="no title" charset="utf-8">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/common.css" media="screen" title="no title" charset="utf-8">
-<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/nav-lp.css" media="screen" title="no title" charset="utf-8">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/nav-lp.css" media="screen" title="no title" charset="utf-8">
 
 
 
