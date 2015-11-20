afd05bd3797164bdf9ecb8c21b2ed0e9f5bacc21
takerusekine <t.0327.s@gmail.com>
2015-11-20 23:40:02 +0900

    LPレイアウトの調整

--- content.php
+++ content.php
@@ -3,7 +3,7 @@
 			<header class="entry-header container-fluid">
 				<div class="row">
 
-				<h1 class="black-text condensed-bold small-heading top-buffer-padding bottom-buffer-padding col-xs-12 col-sm-6">
+				<h1 class="black-text condensed-bold small-heading top-buffer-padding bottom-buffer-padding col-xs-12 col-sm-4">
 					<?php if (is_parent_slug() == 'about-us' || is_parent_slug() == 'service'): ?>
 						<?php
                                 $parent_id = $post->post_parent;
@@ -14,7 +14,7 @@
 				<?php endif; ?>
 				</h1>
 				<?php if (is_page('about-us')||is_parent_slug() == 'about-us'): ?>
-					<div class="col-sm-6 top-buffer-padding bottom-buffer-padding">
+					<div class="col-sm-8 top-buffer-padding bottom-buffer-padding">
 					<?php
                     wp_nav_menu(
                         array(
@@ -26,7 +26,7 @@
                     ;?>
 					</div>
 					<?php elseif (is_page('service')||is_parent_slug() == 'service'): ?>
-						<div class="col-sm-6 top-buffer-padding bottom-buffer-padding">
+						<div class="col-sm-8 top-buffer-padding bottom-buffer-padding">
 						<?php
                                             wp_nav_menu(
                                                     array(
