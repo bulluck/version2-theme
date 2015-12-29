90c43d1947dda04d7903abe3afdfdc5bebced7ec
takerusekine <t.0327.s@gmail.com>
2015-12-29 19:05:49 +0900

    文言の修正と、brタグの追加

--- newlp.php
+++ newlp.php
@@ -280,7 +280,7 @@
                 <div class="col-xs-12 top-buffer-padding purewhitebg">
                   <div class="row">
                     <div class="col-xs-12 col-sm-12">
-                      <h3 class="small-heading black-text font-bold">ランディングページリニューアルにより<span class="redtext">受注率が1.9倍に</span></h3>
+                      <h3 class="small-heading black-text font-bold">LPリニューアルにより<br><span class="redtext">受注率が1.9倍に</span></h3>
                     </div>
                   </div>
                   <hr>
