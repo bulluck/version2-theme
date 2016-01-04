6eabe5c1d21e9a365c692843ae0a1479d5d8bd03
miyagawakazuki <miyagawakazuki@miyagawakazuki-no-MacBook-Air.local>
2016-01-05 04:49:31 +0900

    サイズ修正

--- header.php
+++ header.php
@@ -77,8 +77,8 @@
 												経営コンサルタントによる サイト / LP制作 / SEO対策 / 広告運用
 											</span>
 										<a href="<?php echo home_url('/');?>">
-										 <span class="black-text companyname purewhitebg condensed-bold small-heading" style="padding:0 5px">Web</span>
-										 <span class="white-text companyname condensed-bold small-heading">戦略室</span>
+										 <span class="black-text companyname purewhitebg condensed-bold top-heading" style="padding:0 5px">Web</span>
+										 <span class="white-text companyname condensed-bold top-heading">戦略室</span>
 										 <span class="white-text condensed-normal agreespan bybulluck">produced by Bulluck </span>
 										</a>
 										<?php
